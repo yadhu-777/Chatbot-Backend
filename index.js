695706a5fe8418b5a40141e101b47ecf3a2b837f
yadhu krishna <yadhukris>
2026-01-10 18:51:19 +0530

    addded verify ed pint

--- index.js
+++ index.js
@@ -125,7 +125,8 @@ httpOnly:false,
   res.cookie("auth",token,{
   httpOnly: true,
   secure: false,
-  
+  sameSite: "none",   
+       maxAge: 7 * 24 * 60 * 60 * 1000
   })
  res.send({mesage:"ok",name:name,email:email,thrid:Threds.userId})
   }catch(err){
