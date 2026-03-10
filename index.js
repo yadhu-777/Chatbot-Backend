dc6a4672c97583d8b8de9a55133722d76f499b96
yadhu krishna <yadhukris>
2026-03-10 22:15:33 +0530

    added auht

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors({
   origin:"https://chatbot-frontend-orcin-ten.vercel.app",
   credentials: true,
 }));
+app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
