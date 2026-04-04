edd9437727215e2c339f5b39f0cac81726683e3f
yadhu krishna <yadhukris>
2026-04-04 15:30:52 +0530

    last Update

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post("/classSpec", async (req, res) => {
     const data = await classModel.find({course:course});
   res.json({data:data});
   } catch (err) {
-    res.status(500).send("Error fetching news");
+    res.status(500).send(err);
   }
 });
 app.post("/addclass",upload.single("image"), async (req, res) => {
