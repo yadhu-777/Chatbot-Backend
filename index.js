dc1f9acb6092cd191fad7fde6d4a0a97343fb4c1
yadhu krishna <yadhukris>
2026-04-05 12:27:10 +0530

    last Update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { response } from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import fs from "fs";
 import cors from "cors";
 import OpenAI from "openai";
 import { v4 as uuidv4 } from "uuid";
