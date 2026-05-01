a15cef97180e02094135d10cf07d046fcaabcedf
yadhu krishna <yadhukris>
2026-05-01 09:07:32 +0530

    fi

--- index.js
+++ index.js
@@ -333,15 +333,13 @@ app.get("/news", async (req, res) => {
   }
 });
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4,
 });
+
 app.post("/complaint", upload.single("image"), async (req, res) => {
   try {
     const { subject, description } = req.body;
