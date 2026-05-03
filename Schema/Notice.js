ff31d9f0c5f3241733499bb9be8dae8bbcac8404
yadhu krishna <yadhukris>
2026-05-03 09:15:19 +0530

    fi

--- Schema/Notice.js
+++ Schema/Notice.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 
 const NoticeSchema = new mongoose.Schema({
-  
+  name:String,
     image:String
 })
 const NoticeModel = mongoose.model("NoticeModel",NoticeSchema);
